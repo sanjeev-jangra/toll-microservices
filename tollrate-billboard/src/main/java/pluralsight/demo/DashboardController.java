8de9e70259b893fe231af61109996d1d9fcffa3a
sanjeev-jangra <sanjeev.jangra26@gmail.com>
2019-08-11 17:31:23 -0400

    first upload

---
+++ tollrate-billboard/src/main/java/pluralsight/demo/DashboardController.java
@@ -0,0 +1,33 @@
+package pluralsight.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class DashboardController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@RequestMapping("/dashboard")
+	public String GetTollRate(@RequestParam int stationId, Model m) {
+
+		TollRate tr = restTemplate.getForObject("http://pluralsight-toll-service/tollrate/" + stationId, TollRate.class);
+		System.out.println("stationId: " + stationId);
+		m.addAttribute("rate", tr.getCurrentRate());
+		return "dashboard";
+	}
+}
